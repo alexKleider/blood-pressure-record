087318b5f5b6669f1514163eb4d33d92fb19df7d
Alex Kleider <alexkleider@protonmail.com>
2026-03-27 11:14:23 -0700

    Now use recordBP.py => pressures.txt

--- recordBP.py
+++ recordBP.py
@@ -12,12 +12,22 @@ bp_file = "pressures.txt"
 def append_bp_file():
     from datetime import datetime
     now = datetime.now()
-    timestamp = now.strftime("%a %b %d '%y %H:%M %p")
+    timestamp = now.strftime("%a %b %d: %H:%M")
     #Tuesday, March 24, 2026 at 01:00:57 PM
-    bp_and_pulse = input("Sys/Di Pulse: ")
-    newline = f"\n{timestamp}  {bp_and_pulse}"
+    while True:
+        print("Enter BP and pulse in following format: Sys/Di pulse")
+        bp_and_pulse = input("Sys/Di Pulse: ")
+        data = bp_and_pulse.split()
+        if len(data) != 2: continue
+        pulse = data[1]
+        bp = data[0].split("/")
+        if len(bp) != 2: continue
+        sy, di = bp
+        break
+    newline = f"{timestamp}  {sy}/{di} {pulse}"
     with open(bp_file, 'a') as f:
         f.write(newline)
+    print(f"Appended to {bp_file}: {newline}.")
 
 
 if __name__ == "__main__":
