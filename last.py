18d2859bc97806acb7e273687a4e0cbd7a29611d
Alex Kleider <alexkleider@protonmail.com>
2026-04-11 11:58:22 -0700

    last.py: limits file length keeping header lines

--- last.py
+++ last.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 
-# File: last.py
+# File: new_last.py
 
 """
-reads bps.txt and returns the last 1000 characters
+reads bps.txt and returns content limited to 1,000 characters
+(but retaining the top (header) lines.)
 """
 
-chars_allowed = 1000
+header_length = 127  # the top lines we keep
+chars_allowed = 1000 - header_length
+source_f  = "pressures.txt"
 
 def get_last():
-    with open("bps.txt", 'r') as bps:
-        all_chars = bps.read()
-    allowed_part = all_chars[-chars_allowed:]
-    first_cr = allowed_part.find("\n")
-    return allowed_part[first_cr+1:]
+    with open(source_f, 'r') as bps:
+        headers = bps.read(header_length)
+        readings = bps.read()
+    allowed_part = readings[-chars_allowed:]
+    first_cr = allowed_part.rindex("\n")
+    return headers + allowed_part[:first_cr]
 
 print(get_last())
--- recordBP.py
+++ recordBP.py
@@ -7,7 +7,7 @@ Python equvalent of bash alias bpdate
 but with abreviated year. ('26 vs 2026)
 """
 
-bp_file = "pressures.txt"
+bp_file = "/home/alex/Git/BP/pressures.txt"
 
 def append_bp_file():
     from datetime import datetime
@@ -15,7 +15,7 @@ def append_bp_file():
     timestamp = now.strftime("%a %b %d: %H:%M")
     #Tuesday, March 24, 2026 at 01:00:57 PM
     while True:
-        print("Enter BP and pulse in following format: Sys/Di pulse")
+        print("Enter BP and pulse in format shown:")
         bp_and_pulse = input("Sys/Di Pulse: ")
         data = bp_and_pulse.split()
         if len(data) != 2: continue
@@ -24,10 +24,11 @@ def append_bp_file():
         if len(bp) != 2: continue
         sy, di = bp
         break
-    newline = f"{timestamp}  {sy}/{di} {pulse}"
+    newline = f"{timestamp}  {sy:>3}/{di} {pulse}"
     with open(bp_file, 'a') as f:
-        f.write(newline)
-    print(f"Appended to {bp_file}: {newline}.")
+        print(newline, file=f)
+    print(f"Appended '{newline}' to...")
+    print(f"'{bp_file}'.")
 
 
 if __name__ == "__main__":
