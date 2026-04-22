13e4393718715b6811af1d80c74517486fe597e8
Alex Kleider <alexkleider@protonmail.com>
2026-04-22 11:27:44 -0700

    Added keyword params (with defaults) to last.py

--- last.py
+++ last.py
@@ -3,18 +3,23 @@
 # File: last.py
 
 """
-reads bps.txt and returns content limited to 1,000 characters
+Reads bps.txt and returns content
+limited to <chars_allowed> characters.
 (but retaining the top (header) lines.)
+<chars_allowed> defaults to 1000.
 """
 
 header_length = 127  # the top lines we keep
 chars_allowed = 1000 - header_length
 source_f  = "pressures.txt"
 dest_f = "limited_pressures.txt"
-spaces_to_indent = 6
-indent = " " * 6 
 
-def get_last():
+def get_last(source_f=source_f, 
+             dest_f = dest_f,
+             header_length=127,
+             chars_allowed=chars_allowed,
+             spaces2indent=0):
+    indent = " " * spaces2indent 
     with open(source_f, 'r') as bps:
         headers = bps.read(header_length)
         readings = bps.read()
@@ -22,10 +27,19 @@ def get_last():
     first_cr = allowed_part.rindex("\n")
     txt = headers + allowed_part[:first_cr]
     yn = input(f"Send to {dest_f}? (yn): ")
-    if yn and yn[0] in "yY":
-        with open(dest_f, 'w') as outf:
-            for line in txt.split("\n"):
-                print(line, file=outf)
+    if yn and yn[0] in "nN":
+        alt_dest = input(
+            "Enter alterate destination file (or blank): ")
+        if not alt_dest:
+            return txt
+        else:
+            if alt_dest == source_f:
+                print(f"Bad to destroy {source_f}!!!")
+                return
+            dest_f = alt_dest
+    with open(dest_f, 'w') as outf:
+        for line in txt.split("\n"):
+            print(line, file=outf)
     return txt
 
 print(get_last())
