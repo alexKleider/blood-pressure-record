296b75b628188f48ab0c97108a6816778ee57578
Alex Kleider <alexkleider@protonmail.com>
2026-05-01 14:34:09 -0700

    last.py working properly

--- last.py
+++ last.py
@@ -10,22 +10,41 @@ limited to <chars_allowed> characters.
 """
 
 header_length = 127  # the top lines we keep
-chars_allowed = 1000 - header_length
+    # being replaced by n_header_lines
+salutation = """Most recent readings...
+"""
 source_f  = "pressures.txt"
-dest_f = "limited_pressures.txt"
+dest_f = "limited_BPs.txt"
+chars_allowed = 1000 - header_length -len(salutation)
+n_header_lines = 4  # number of header
+                    # lines to preserve.
 
-def get_last(source_f=source_f, 
-             dest_f = dest_f,
-             header_length=127,
+def get_last(source_f,
+             dest_f=dest_f,
+             n_header_lines=4,
+             n_new_header = salutation,
              chars_allowed=chars_allowed,
              spaces2indent=0):
+    def send2file(txt, dest_f):
+        if dest_f == source_f:
+            print(f"Bad to destroy {source_f}!!!")
+            return
+        else:
+            with open(dest_f, 'w') as outf:
+                print(txt, file=outf)
+
     indent = " " * spaces2indent 
     with open(source_f, 'r') as bps:
-        headers = bps.read(header_length)
+        headers = []
+        while len(headers) < n_header_lines:
+            headers.append(bps.readline())
         readings = bps.read()
-    allowed_part = readings[-chars_allowed:]
-    first_cr = allowed_part.rindex("\n")
-    txt = headers + allowed_part[:first_cr]
+    chars_allowed = chars_allowed-len(n_new_header)
+#   allowed_part = readings[-(chars_allowed-3):]
+    remaining_part = readings[-(chars_allowed):]
+    first_cr = remaining_part.find("\n") + 1
+    remaining_part = remaining_part[first_cr:]
+    txt = "".join(headers) + remaining_part
     yn = input(f"Send to {dest_f}? (yn): ")
     if yn and yn[0] in "nN":
         alt_dest = input(
@@ -33,13 +52,23 @@ def get_last(source_f=source_f,
         if not alt_dest:
             return txt
         else:
-            if alt_dest == source_f:
-                print(f"Bad to destroy {source_f}!!!")
-                return
-            dest_f = alt_dest
-    with open(dest_f, 'w') as outf:
-        for line in txt.split("\n"):
-            print(line, file=outf)
+            send2file(salutation+txt, alt_dest)
+    else:
+        send2file(salutation+txt, dest_f)
     return txt
 
-print(get_last())
+def ck_get_last():
+    n2indent = input("Spaces to indent? (blank==0): ")
+    if not n2indent: n2indent=0
+    else: n2indent = int(n2indent)
+    ret = get_last(source_f, dest_f,
+                   spaces2indent=n2indent)
+    yn = input(
+            "Send result to terminal? (y/n): ")
+    if yn and yn[0] in "yY":
+        for line in ret:
+            print(line)
+
+if __name__ == "__main__":
+    ck_get_last()
+
